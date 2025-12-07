c270b3fa5f0d680027bfcc79f080a3100388c413
Vx-tech <xaviervianney8@gmail.com>
2025-12-07 16:53:50 +0300

    Tracking an error in Layout folder

--- alx-movie-app/components/commons/MovieCard.tsx
+++ alx-movie-app/components/commons/MovieCard.tsx
@@ -2,7 +2,7 @@ const MovieCard: React.FC<MovieCard> = ({ imageUrl, prompt, width, action }) => 
   return (
     <div onClick={() => action(imageUrl)} className="mt-6 border hover:cursor-pointer">
       <img src={imageUrl} alt={prompt} className={`w-full max-w-md rounded-lg shadow-lg`} />
-      <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Your Prompt:</h2>
+      <h2 className={`${width ? 'text-sm' : 'text-xl'} font-semibold mt-2`}>Yours Prompt:</h2>
       <p className={`${width ? 'text-xs' : 'text-lg'} text-gray-700 mb-4`}>{prompt}</p>
     </div>
   )
